d1427ce2d23f465786b26c3e5db80193f037aa84
Yen Nhi Ho <yen.nhi.ho@iem.thm.de>
2020-01-04 18:36:47 +0100

    First commit

---
+++ index.js
@@ -0,0 +1,52 @@
+var Crawler = require("crawler");
+
+var c = new Crawler({
+  maxConnections: 10,
+  // This will be called for each crawled page
+  callback: function(error, res, done) {
+    if (error) {
+      console.log(error);
+    } else {
+      const products = [];
+      var $ = res.$;
+
+      prodcts = setProducts($, products);
+
+      console.log(products);
+      console.log(products.length);
+    }
+    done();
+  }
+});
+
+function setProducts($, products) {
+  //set name and id of product
+  $(".products-wp li a").each(function(i, element) {
+    const $element = $(element);
+    let idCurrent = $element
+      .attr("data-ui-name")
+      .match(/aues\.product\.(.*)\.li/)[1];
+    let nameCurrent = $element.attr("href").match(/(.*)\/(.*)\/(.*)\/(.*)/)[2];
+    const product = {
+      name: nameCurrent,
+      id: idCurrent
+    };
+    products.push(product);
+  });
+
+  //set price of product
+  $(".info-container .price .price-new").each(function(i, element) {
+    const $element = $(element);
+    let priceCurrent = $element.attr("data-csscontent");
+    products[i].price = priceCurrent;
+  });
+
+  return products;
+}
+
+// Queue a list of URLs
+c.queue([
+  "https://www.obi.de/eisenwaren-beschlaege/schrauben/c/2440",
+  "https://www.obi.de/elektrogeraete/kochfelder/c/4442",
+  "https://www.obi.de/moebel/betten/c/2225"
+]);
